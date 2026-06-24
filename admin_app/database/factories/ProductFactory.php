ed59020db03eb4dc81097f199d90c70440b7d813
Kamruzzaman-98 <kamruzzamank98@gmail.com>
2026-06-24 11:35:42 +0600

    add ProductFactory

---
+++ admin_app/database/factories/ProductFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'name'  => fake()->words(3, true),
+            'sku'   => strtoupper(fake()->unique()->bothify('SKU-#####')),
+            'price' => fake()->randomFloat(2, 10, 5000),
+        ];
+    }
+}
