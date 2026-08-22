d4eb088262800d5c80a5fd2685ff0c30d7deff07
Kamruzzaman-98 <kamruzzamank98@gmail.com>
2026-08-22 14:55:14 +0600

    add PermissionSeeder

---
+++ admin_app/database/seeders/PermissionSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $permissions = [
+            'access admin',
+            'view dashboard',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate([
+                'name' => $permission,
+                'guard_name' => 'web',
+            ]);
+        }
+    }
+}
