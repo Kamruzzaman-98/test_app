5cc0fbbd45e9423815cbd13b88521db8d1156347
Kamruzzaman-98 <kamruzzamank98@gmail.com>
2026-08-22 15:46:28 +0600

    update and run

--- admin_app/database/seeders/DatabaseSeeder.php
+++ admin_app/database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Contracts\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,6 +22,9 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             ProductSeeder::class,
+            RoleSeeder::class,
+            PermissionSeeder::class,
+            UserSeeder::class
         ]);
 
         User::factory()->create([
